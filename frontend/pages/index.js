563f3c14b1584a747e149ca42937aaa220e6560c
MAlngaawy <m.alngaawy@gmail.com>
2020-08-01 21:21:49 +0200

    test GSP async fun

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,14 +1,23 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({name}) {
   return (
     <div className={styles.container}>
       <Head>
         <title>Blog App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Hello To My Blog</h1>
+      <h1>Hello To {name} Blog</h1>
     </div>
   )
+}
+
+
+export async function getStaticProps(context) {
+  return {
+    props: {
+      name: 'Alngaawy'
+    }, // will be passed to the page component as props
+  }
 }
