770f425b993a88dd2be0e9b41c952dc5324718c7
MAlngaawy <m.alngaawy@gmail.com>
2020-08-01 21:34:29 +0200

    add blog CT and fetch it

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home({name}) {
+export default function Home({name, blogs}) {
   return (
     <div className={styles.container}>
       <Head>
@@ -9,15 +9,22 @@ export default function Home({name}) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Hello To {name} Blog</h1>
+      {blogs.map(blog => {
+        return <h2>{blog.title}</h2>
+      })}
     </div>
   )
 }
 
 
 export async function getStaticProps(context) {
+
+  const res = await fetch('http://localhost:1337/blogs')
+  const blogs = await res.json();
   return {
     props: {
-      name: 'Alngaawy'
+      name: 'Alngaawy',
+      blogs
     }, // will be passed to the page component as props
   }
 }
